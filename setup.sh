39871283a479a83261b8bd839fb4b6e25b2a4c4e
Todor Mihaylov <tbmihailov@gmail.com>
2018-09-27 16:21:43 +0200

    Added requirements and script for setup

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+
+
+mkdir resources
+cd resources
+wget https://github.com/explosion/sense2vec/releases/download/v1.0.0a0/reddit_vectors-1.1.0.tar.gz
+tar -xzf reddit_vectors-1.1.0.tar.gz
